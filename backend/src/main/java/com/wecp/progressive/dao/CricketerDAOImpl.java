0649666b078e9abb9d49080423ed12c94cf0d58e
username <wecp-dummy@gmail.com>
2024-12-30 13:24:32 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/dao/CricketerDAOImpl.java
@@ -0,0 +1,70 @@
+package com.wecp.progressive.dao;
+
+import java.util.List;
+
+import com.wecp.progressive.entity.Cricketer;
+
+public class CricketerDAOImpl implements CricketerDAO{
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    @Override
+    public int addCricketer(Cricketer cricketer) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void deleteCricketer(int cricketerId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Cricketer> getAllCricketers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Cricketer getCricketerById(int cricketerId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateCricketer(Cricketer cricketer) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+}
---
+++ backend/src/main/java/com/wecp/progressive/service/impl/TicketBookingServiceImpl.java
@@ -0,0 +1,66 @@
+package com.wecp.progressive.service.impl;
+
+import java.util.List;
+
+import com.wecp.progressive.entity.TicketBooking;
+import com.wecp.progressive.service.TicketBookingService;
+
+public class TicketBookingServiceImpl implements TicketBookingService {
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    @Override
+    public void cancelBooking(int bookingId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int createBooking(TicketBooking ticketBooking) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<TicketBooking> getAllTicketBookings() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<TicketBooking> getBookingsByUserEmail(String email) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
+
+}
