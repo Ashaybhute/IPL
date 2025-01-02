bb7ed137df0c41f180894e21655134b0fb01569d
username <wecp-dummy@gmail.com>
2025-01-02 14:45:07 +0000

    Day5

--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplArraylist.java
@@ -3,10 +3,13 @@ package com.wecp.progressive.service.impl;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
- 
+
+import org.springframework.stereotype.Service;
+
 import com.wecp.progressive.entity.Team;
 import com.wecp.progressive.service.TeamService;
  
+@Service
 public class TeamServiceImplArraylist implements TeamService {
  
     private List<Team> teamList = new ArrayList<>();
